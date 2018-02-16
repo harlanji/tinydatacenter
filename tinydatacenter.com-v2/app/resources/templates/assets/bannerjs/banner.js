08250f6b5bf9f25d5160ddad782c4e59a17f3376
Harlan Iverson <h.iverson@gmail.com>
2018-02-16 19:08:45 +0000

    added bannerjs to layout for tinydc

---
+++ tinydatacenter.com-v2/app/resources/templates/assets/bannerjs/banner.js
@@ -0,0 +1,37 @@
+doBanner();
+
+
+function doBanner () {
+  if (window.sessionStorage) {
+    var s = window.sessionStorage;
+
+    if (s.getItem("banner-cleared") != "true") {
+      installBanner();
+    }
+  } else {
+    installBanner();
+  }
+}
+
+
+
+function installBanner () {
+  var banner = document.createElement("div");
+
+  banner.setAttribute("id", "banner");
+  banner.setAttribute("style", "background-color: yellow; font-weight: bold; color: black; width: 100%; height: 30px; text-align: center");
+
+  banner.textContent = "Stay tuned for updates..."
+
+  document.body.insertBefore(banner, document.body.firstChild);
+
+  banner.addEventListener("click", function () {
+    document.body.removeChild(banner);
+    banner = null;
+
+    if (window.sessionStorage) {
+      var s = window.sessionStorage;
+      s.setItem("banner-cleared", "true");
+    }
+  });
+}
