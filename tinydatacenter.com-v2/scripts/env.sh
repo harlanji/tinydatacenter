7bb63eac859293fc8ea7b06940df432d3008d05c
Harlan Iverson <h.iverson@gmail.com>
2018-03-06 18:56:13 +0000

    updated tdc scripts

--- tinydatacenter.com-v2/scripts/env.sh
+++ tinydatacenter.com-v2/scripts/env.sh
@@ -5,4 +5,8 @@ NAME=tinydatacenter-build
 
 DEV_PORT=23000
 
+NAME_STATIC=tinydatacenter-static
+IMAGE_STATIC=docker-registry.local:5000/tinydatacenter.com/tinydatacenter-static:1
+PORT_STATIC=23080
+
 MEDIA=$HOME/p/tdc-media
--- tinydatacenter.com-v2/scripts/start-static.sh
+++ tinydatacenter.com-v2/scripts/start-static.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --restart always  --name website-v2-static -p 23080:9090 website-v2-static
+source scripts/env.sh
+
+
+docker run -d --restart unless-stopped \
+  --name $NAME_STATIC \
+  -p $PORT_STATIC:9090 \
+  -v $MEDIA:/tmp/http/media:ro \
+  $IMAGE_STATIC
