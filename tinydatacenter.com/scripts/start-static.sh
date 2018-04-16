6a8403078edeb5aa4699fdf175d2e46f324942dd
Harlan Iverson <h.iverson@gmail.com>
2018-04-16 18:01:54 +0000

    updated start static script to reflect new http storage location

--- tinydatacenter.com/scripts/start-static.sh
+++ tinydatacenter.com/scripts/start-static.sh
@@ -6,5 +6,5 @@ source scripts/env.sh
 docker run -d --restart unless-stopped \
   --name $NAME_STATIC \
   -p $PORT_STATIC:9090 \
-  -v $MEDIA:/tmp/http/media:ro \
+  -v $MEDIA:/var/www/html/media:ro \
   $IMAGE_STATIC
